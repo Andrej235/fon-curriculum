025b2cc2e57f1c1be05e2fba528b437f9fb5607e
andrej <129008667+Andrej235@users.noreply.github.com>
2025-10-29 17:52:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## This project was created with andrej235/frontend-template
+
+It is just a NextJS app created using create-next-app with setup theme provider, shadcn and prettier tailwind plugin
---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { ThemeProvider } from "next-themes";
+import { Roboto, Roboto_Slab } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin"],
+});
+
+const robotoSlab = Roboto_Slab({
+  variable: "--font-roboto-slab",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "My Website",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${roboto.variable} ${robotoSlab.variable} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
