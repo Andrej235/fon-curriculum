63286201b0f3a55d7f57b78abcba42e669a8825f
Andrej235 <andrej@nenadic.dev>
2025-10-30 10:33:08 +0100

    Add column highlighting

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -17,10 +17,14 @@ import {
 } from "@/components/ui/table";
 import { curriculum } from "@/lib/curriculum";
 import { days } from "@/lib/day";
+import { cn } from "@/lib/utils";
 import { useMemo, useState } from "react";
 
 export default function Home() {
   const [selectedGroup, setSelectedGroup] = useState<string>("A1");
+  const [hoveringOverColumn, setHoveringOverColumn] = useState<number | null>(
+    null,
+  );
 
   const curriculumTimes = useMemo(
     () =>
@@ -61,12 +65,31 @@ export default function Home() {
         </div>
       </header>
 
-      <Table className="*:w-[max(80vw,80vh)] min-w-0">
+      <Table className="overflow-auto select-none">
         <TableHeader>
           <TableRow>
-            <TableHead>Vreme</TableHead>
-            {days.map((day) => (
-              <TableHead key={day}>{day}</TableHead>
+            <TableHead
+              className={cn(
+                "text-center transition-colors",
+                hoveringOverColumn === 0 && "bg-muted/40",
+              )}
+              onMouseOver={() => setHoveringOverColumn(0)}
+              onMouseLeave={() => setHoveringOverColumn(null)}
+            >
+              Vreme
+            </TableHead>
+            {days.map((day, index) => (
+              <TableHead
+                className={cn(
+                  "text-center transition-colors",
+                  hoveringOverColumn === index + 1 && "bg-muted/40",
+                )}
+                key={day}
+                onMouseEnter={() => setHoveringOverColumn(index + 1)}
+                onMouseLeave={() => setHoveringOverColumn(null)}
+              >
+                {day}
+              </TableHead>
             ))}
           </TableRow>
         </TableHeader>
@@ -83,7 +106,16 @@ export default function Home() {
 
             return Array.from({ length: max }).map((_, i) => (
               <TableRow key={`${i}-${time}`}>
-                <TableCell>{time}</TableCell>
+                <TableCell
+                  className={cn(
+                    "text-center transition-colors",
+                    hoveringOverColumn === 0 && "bg-muted/40",
+                  )}
+                  onMouseEnter={() => setHoveringOverColumn(0)}
+                  onMouseLeave={() => setHoveringOverColumn(null)}
+                >
+                  {time}
+                </TableCell>
 
                 {classes.map((x, j) => (
                   <TableCell
@@ -92,15 +124,25 @@ export default function Home() {
                         ? `${i}-${j}-empty`
                         : `${i}-${x[i].groups}-${x[i].location}-${x[i].subject}`
                     }
-                    className="h-32 max-w-max"
+                    className={cn(
+                      "h-32 max-w-max text-center transition-colors",
+                      hoveringOverColumn === j + 1 && "bg-muted/40",
+                    )}
+                    onMouseEnter={() => setHoveringOverColumn(j + 1)}
+                    onMouseLeave={() => setHoveringOverColumn(null)}
                   >
                     {x[i] && (
                       <div className="px-6">
                         <h3 className="text-center text-sm">{x[i].subject}</h3>
+                        <div className="flex justify-between">
+                          <p className="mt-2 text-end text-xs text-muted-foreground">
+                            {x[i].type}
+                          </p>
 
-                        <p className="mt-2 text-end text-xs text-muted-foreground">
-                          {x[i].location}
-                        </p>
+                          <p className="mt-2 text-end text-xs text-muted-foreground">
+                            {x[i].location}
+                          </p>
+                        </div>
                       </div>
                     )}
                   </TableCell>
