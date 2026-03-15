c14caaf43dc4bca571a406d8d52785dce1595dcf
Andrej235 <andrej@nenadic.dev>
2026-03-15 12:03:47 +0100

    Add a proxy to redirect all not found requests to /raspored

---
+++ website/proxy.ts
@@ -0,0 +1,14 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export async function proxy(request: NextRequest) {
+  const allowedRoutes = ["/", "/raspored", "/raspored/"];
+  if (allowedRoutes.includes(request.nextUrl.pathname)) {
+    return NextResponse.next();
+  }
+
+  return NextResponse.redirect(new URL("/raspored", request.url));
+}
+
+export const config = {
+  matcher: "/((?!_next/static|_next/image|favicon.ico).*)",
+};
