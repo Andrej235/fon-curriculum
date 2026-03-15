447794bab4a8b0932dc9163c7d9e7b7ee491198a
Andrej235 <andrej@nenadic.dev>
2026-03-15 11:49:35 +0100

    Update curriculum

--- get-curriculum/src/main.ts
+++ get-curriculum/src/main.ts
@@ -1,10 +1,12 @@
 import fs from "fs";
 import * as pdfjsLib from "pdfjs-dist/legacy/build/pdf.mjs";
 
-const url: string = `https://oas.fon.bg.ac.rs/wp-content/uploads/2025/11/PrvaGodinaZimski25-26.pdf`;
+const url: string = `https://oas.fon.bg.ac.rs/wp-content/uploads/2026/03/Letnji-2025-26-1-15032026.pdf`;
 const days = ["Ponedeljak", "Utorak", "Sreda", "Četvrtak", "Petak"];
 
 const pdfData = await extractPdfText(url);
+
+if (process.argv.includes("raw")) fs.writeFileSync("raw.txt", pdfData);
 
 const curriculum: {
   [key in string]?: {
@@ -34,19 +36,26 @@ for (let i = 0; i < dayIndices.length; i++) {
   dayText = dayText.slice(dayName.length).trim();
 
   const regex =
-    /(.+?)   ([P,V])   (A\d+(?:,\s*A\d+)*)   (\d\d?:\d\d? ?- ?\d\d?:\d\d?)   (.+?)(?:(?<=\S) {2}(?=\S))/g;
+    /(.+?)   ([P,V])   ([A-D]\d+(?:,\s*[A-D]\d+)*)   ?(\d\d?:\d\d? {0,2}- {0,2}\d\d?:\d\d?)   ?(.+?)(?:(?<=\S) {2}(?=\S))/g;
   const matches = dayText
     .matchAll(regex)
     .map(([, subject, type, groups, time, location]) => ({
       subject: subject.replace(/\s+/g, " ").trim(),
       type,
-      groups: groups.split(",").map((g) => g.trim()),
+      groups: groups
+        .split(",")
+        .map((g) => g.trim())
+        .filter((g) => g.startsWith("A")),
       time,
       location,
     }))
     .toArray();
 
   curriculum[dayName] = matches;
+}
+
+for (const day of days) {
+  if (!curriculum[day]) curriculum[day] = [];
 }
 
 fs.writeFileSync("output.json", JSON.stringify(curriculum));
