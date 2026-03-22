d426227b79ad319e6f85e008b5afce833bee0490
Andrej235 <andrej.nenadic06@gmail.com>
2026-03-22 20:39:00 +0100

    Fix robots and sitemap being redirected to root

--- website/lib/base-url.ts
+++ website/lib/base-url.ts
@@ -1,2 +1,5 @@
-export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL!;
+export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL?.trim().replace(
+  /\/+$/,
+  "",
+) as string; // Remove trailing slashes
 if (!baseUrl) throw new Error("NEXT_PUBLIC_BASE_URL is not defined");
